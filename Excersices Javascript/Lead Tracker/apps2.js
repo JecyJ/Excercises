4c30644fffd236f95d9caaeb398c5836237e23f9
Jecy Jehu <jecyjehu@gmail.com>
2022-11-15 14:54:45 +0100

    update

--- Excersices Javascript/Lead Tracker/apps2.js
+++ Excersices Javascript/Lead Tracker/apps2.js
@@ -1,30 +1,49 @@
 const inputEl = document.querySelector('.input-el');
 const btn = document.querySelector('.input-btn');
 const ulEl = document.querySelector('.ul-el');
+const deleteBtn = document.querySelector('.delete-btn');
 let myLeads = [];
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+const saveBtn = document.querySelector('.save-btn');
+
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = '';
+    for (let i = 0; i < leads.length; i++) {       
+        listItems +=`
+        <li>
+            <a target = "_blank" href = ${leads[i]}>
+                ${leads[i]}
+            </a>
+        </li>`;        
+    }
+    ulEl.innerHTML = listItems
+}
 
 
 btn.addEventListener('click', () => {
    myLeads.push(inputEl.value);
    inputEl.value = '';
-   renderLeads();
+   localStorage.setItem('myLeads', JSON.stringify(myLeads));   
+   render(myLeads); 
 })
 
-function renderLeads() {
-    let listItems = '';
-    for (let i = 0; i < myLeads.length; i++) {
-        // (first way)
-        listItems +=`
-        <li>
-            <a target = "_blank" href = ${myLeads[i]}>
-                ${myLeads[i]}
-            </a>
-        </li>`;
-        // (second way. best practise)
-        // const li = document.createElement("li")
-        // li.innerHTML = myLeads[i];
-        // ulEl.append(li);
-    }
-    ulEl.innerHTML = listItems
-}
-
+saveBtn.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tab) => {
+        myLeads.push(tab[0].url);     
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));   
+        render(myLeads);
+     });
+    
+})
+deleteBtn.addEventListener('dblclick', () => {
+    localStorage.clear()
+    myLeads = [];
+    render(myLeads)
+})
